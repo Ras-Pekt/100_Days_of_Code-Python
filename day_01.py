9e000d9cc29d7cfd998c14c580c273fa7f992f5e
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-13 14:23:49 +0300

    band name generator

---
+++ day_01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Band name generator program
+"""
+
+print("Welcome to the Band Name Generator.")
+
+while True:
+    city_name = input("What's the name of the city you grew up in? ")
+    if (city_name == ""):
+        print("Please enter a name")
+    else:
+        break
+
+while True:
+    pet_name = input("What's your pet's name? ")
+    if (pet_name == ""):
+        print("please enter a name")
+    else:
+        break
+
+print(f"Your band name could be {city_name} {pet_name}")
