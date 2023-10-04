29f5e0bbaa80afd114eed715a7475c21d8dade0d
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-10-04 21:58:32 +0300

    day_09

---
+++ Day_09/secret_auction/secret_auction.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from art import logo
+
+"""
+This exercise requires the screen to be cleared once a bidder is done bidding
+However, since this is for learning purposes
+(and there isn't an straightforward way to accomplish this the terminal)
+this feature will not be included
+"""
+
+print(logo, "\n\n")
+
+print("Welcome to the secret auction program.")
+
+bid_dict = {}
+
+while True:
+    
+    while True:
+        bidder_name = input("What is your name?: ").lower()
+        if not bidder_name:
+            continue
+        break
+
+    while True:
+        try:
+            bid = int(input("What is your bid?: $"))
+            break
+        except Exception as e:
+            continue
+
+    bid_dict[bidder_name] = bid
+
+    prompt = input("Are there any other bidders: Type 'Yes' or 'No'\n").lower()
+    if not prompt or prompt != "yes":
+        break
+
+highest_bid = max(bid_dict.values())
+highest_bidder = max(bid_dict, key=bid_dict.get)
+
+print(f"The winner is {highest_bidder.title()} with a bid of ${highest_bid}.")
